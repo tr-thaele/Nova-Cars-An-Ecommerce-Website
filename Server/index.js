be2ba8f8579810ec0cd7ff256b46691ccb7987c5
dagmfre <96683816+dagmfre@users.noreply.github.com>
2024-01-04 19:35:21 +0300

    Setting the CORS manually in index.js

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
-const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
@@ -22,10 +21,26 @@ app.use(express.static("public"));
 
 app.use(express.json());
 
-const corsOptions = {
-  origin:"http://localhost:3000",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 const formDataSchema = new mongoose.Schema({
   category: String,
